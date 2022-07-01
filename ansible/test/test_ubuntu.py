fd8124762e9e4a8f5bf971c90ba78cb10ad5ebe3
dev-kimdoyoung <dev-kimyodouny@github.com>
2022-07-01 15:28:25 +0900

    디렉토리 구조 및 CI 코드 변경
    1. Travis CI 코드 변경
    2. 환경 별 앤서블 플레이북 구분

---
+++ ansible/test/test_ubuntu.py
@@ -0,0 +1,34 @@
+import unittest
+import testinfra
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.host = testinfra.get_host("local://", sudo=True)
+
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+
+    def test_exist_account_group(self):
+        bastion_user = self.host.user("bastion")
+        wheel = self.host.group("wheel")
+        bastion = self.host.group("bastion")
+        
+        self.assertTrue(wheel.exists)
+        self.assertTrue(bastion.exists)
+        self.assertTrue(bastion_user.exists)
+
+
+    # def test_systemd_services(host):
+    #     assert host.package('ntpd').is_installed == True
+    #     ntp = host.service('ntp')
+    #     assert ntp.is_running == True
+    #     assert ntp.is_enabled == True
+
+
+if __name__ == "__main__":
+    unittest.main()
+
